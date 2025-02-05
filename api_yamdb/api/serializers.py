4ed8b0e629a74d09c3ae73413f07d1afd1c4e2e4
Ramil Shagiev <shrr2006@mail.ru>
2025-02-05 17:26:45 +0400

    added serializers and views for comments and reviews

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from reviews.models import Comment, Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('review',)
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+        read_only_fields = ('title',)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from .constants import DISPLAYED_TEXT
+
 
 class User(models.Model):
     pass
@@ -18,8 +20,30 @@ class Genre(models.Model):
 
 
 class Review(models.Model):
-    pass
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews'
+    )
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True, db_index=True
+    )
 
 
 class Comment(models.Model):
-    pass
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True, db_index=True
+    )
+
+    def __str__(self):
+        return self.text[:DISPLAYED_TEXT]
