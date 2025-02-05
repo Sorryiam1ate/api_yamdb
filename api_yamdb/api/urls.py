8f58ab5f8678acb27f54924794a71422c9c25c0f
Ramil Shagiev <shrr2006@mail.ru>
2025-02-05 16:38:58 +0400

    add urls, views, serializers, permissions

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (CommentViewSet, CategoryViewSet,
+                    GenreViewSet, ReviewViewSet, TitleViewSet)
+
+
+router_v1 = DefaultRouter()
+router_v1.register('categories', CategoryViewSet, basename='category')
+router_v1.register('genres', GenreViewSet, basename='genre')
+router_v1.register('titles', TitleViewSet, basename='title')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   ReviewViewSet, basename='review')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comment'
+)
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls))
+]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+class TitleViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    pass
